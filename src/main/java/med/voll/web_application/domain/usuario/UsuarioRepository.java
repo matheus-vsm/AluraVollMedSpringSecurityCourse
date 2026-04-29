090b73f4c4bd91c21d294e70fc938bfcd0ee0c0b
matheus-vsm <scompinha@hotmail.com>
2026-04-28 23:27:07 -0300

    aula 4.4

---
+++ src/main/java/med/voll/web_application/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package med.voll.web_application.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmailIgnoreCase(String email);
+
+}
---
+++ src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -0,0 +1,21 @@
+package med.voll.web_application.domain.usuario;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UsuarioService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override // metodo de busca do usuario
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmailIgnoreCase(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+    }
+
+}
