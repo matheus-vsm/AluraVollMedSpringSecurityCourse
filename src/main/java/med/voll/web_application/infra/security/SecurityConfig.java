49c12684c773a67206fba4452e18b71aa17cc2af
matheus-vsm <scompinha@hotmail.com>
2026-04-29 23:40:01 -0300

    aula 4.6

--- src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
+++ src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -46,6 +48,11 @@ public class SecurityConfig {
                 )
                 .csrf(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder codificadorDeSenha() {
+        return new BCryptPasswordEncoder();
     }
 
 }
